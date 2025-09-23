77e722bf4b4da68e943be92a98e13c66d13e5b3d
Joao Bosco Barros Junior <barros.junior@aluno.unb.br>
2025-09-23 17:21:46 -0300

    Cria jframe

--- src/Financeiro.java
+++ src/Financeiro.java
@@ -1,5 +1,18 @@
 import javax.swing.*;
 
-public class Financeiro {
+public class Financeiro extends JFrame {
+    public Financeiro() {
+        setTitle("Financeiro");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+    }
     private JTextField textField1;
+    private JFormattedTextField formattedTextField1;
+
+    public static void main(String[] args) {
+        new Financeiro();
+    }
 }
