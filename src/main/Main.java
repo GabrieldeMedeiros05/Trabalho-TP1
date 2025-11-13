cc3e19c171ac528deda1442535a20f92e32887b5
Joao Bosco Barros Junior <barros.junior@aluno.unb.br>
2025-11-12 21:52:28 -0300

    Ajusta a Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,29 +2,15 @@ package main;
 
 import javax.swing.*;
 
-import Financeiro.dados.LeitorPessoa;
-import Financeiro.dados.LeitorFuncionario;
-import Financeiro.ui.TelaFinanceiro;
-import Financeiro.ui.TelaFolhaPagamento;
-import Seguranca.dominio.Funcionario;
-import Seguranca.dominio.Pessoa;
 import main.ui.TelaLogin;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
-        List<Funcionario> funcionarios = new LeitorFuncionario().retornaLista();
-        for (Pessoa funcionario: funcionarios) {
-            System.out.println(funcionario);
-        }
-
-        JFrame tela = new TelaFinanceiro();
-        tela.setVisible(true);
-//        SwingUtilities.invokeLater(() -> {
-//            TelaLogin telaLogin = new TelaLogin();
-//            telaLogin.setVisible(true);
-//        });
+        SwingUtilities.invokeLater(() -> {
+            TelaLogin telaLogin = new TelaLogin();
+            telaLogin.setVisible(true);
+        });
     }
 }
